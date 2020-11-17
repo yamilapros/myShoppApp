14907ca57f767f1982f0a3bc5489062ac5139181
yamila <yamiprosdocimo@gmail.com>
2020-11-17 08:35:39 +0100

    Request

--- public/index.php
+++ public/index.php
@@ -48,8 +48,8 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 
 $kernel = $app->make(Kernel::class);
 
-$response = tap($kernel->handle(
-    $request = Request::capture()
-))->send();
+$response = $kernel->handle(
+    $request = App\Custom\Http\Request::capture()
+);
 
 $kernel->terminate($request, $response);
